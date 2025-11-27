5afb6ec8a9b760481ccdf2677d62e91be7236ec5
Favour John <fav.dev.web@gmail.com>
2025-11-27 23:51:48 +0100

    Fix backend auth or CORS

--- index.js
+++ index.js
@@ -37,6 +37,9 @@ app.use(
     origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    SameSite: "None",
+    Secure: true, 
   })
 );
 
